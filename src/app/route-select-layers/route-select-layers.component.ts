c5838cf73b7a2c2d32c36b28f99ccc58b1ed5b09
Sage Wall <sagewall@users.noreply.github.com>
2019-09-11 15:22:30 -0600

    Add setter for map layer

--- src/app/route-select-layers/route-select-layers.component.ts
+++ src/app/route-select-layers/route-select-layers.component.ts
@@ -7,12 +7,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RouteSelectLayersComponent implements OnInit {
 
-  category: string;
-  layer: string;
+  private layer: string;
+  private mapLayer: any;
+
+  selectedCategory: string;
+
+  set selectedLayer(value) {
+    this.layer = value;
+
+    switch (value) {
+      case 'soils': {
+        this.mapLayer = 'https://landscape11.arcgis.com/arcgis/rest/services/USA_Soils_Map_Units/mapserver';
+        break;
+      }
+      case 'surfaceGeology': {
+        this.mapLayer = 'https://landscape1.arcgis.com/arcgis/rest/services/USA_Geology_Units/MapServer';
+        break;
+      }
+      case 'surfaceWater': {
+        this.mapLayer = 'https://landscape2.arcgis.com/arcgis/rest/services/USA_Surface_Water/ImageServer';
+        break;
+      }
+      case 'aquifers': {
+        this.mapLayer = 'https://landscape2.arcgis.com/arcgis/rest/services/USA_Surface_Water/ImageServer';
+        break;
+      }
+      default: {
+        this.mapLayer = '';
+        break;
+      }
+    }
+
+    this.switchMapLayer(value);
+  }
+
+  get selectedLayer(): string {
+    return this.layer;
+  }
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  switchMapLayer(value) {
+    console.log(this.mapLayer);
   }
 
 }
